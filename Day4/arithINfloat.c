c0c00f46995bb4ab0ed47e5b463fbef68948f343
ankitkamani <100353312+ankitkamani@users.noreply.github.com>
2023-02-10 16:48:13 +0530

    Add files via upload

---
+++ Day4/arithINfloat.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+void main(){
+
+float a,b,c;
+
+printf("sum of two number :");
+
+printf("Enter first number :-");
+scanf("%f",&a);
+
+printf("Enter second number :-");
+scanf("%f",&b);
+
+c = a+b;
+printf("sum of two number is :-%f",c);
+
+
+printf("substraction of two number :");
+
+printf("Enter first number :-");
+scanf("%f",&a);
+
+printf("Enter second number :-");
+scanf("%f",&b);
+
+c = a-b;
+printf("substraction of two number is :-%f",c);
+
+
+printf("multipication of two number :");
+
+printf("Enter first number :-");
+scanf("%f",&a);
+
+printf("Enter second number :-");
+scanf("%f",&b);
+
+c = a*b;
+printf("multipication of two number is :-%f",c);
+
+
+printf("divisiom of two number :");
+
+printf("Enter first number :-");
+scanf("%f",&a);
+
+printf("Enter second number :-");
+scanf("%f",&b);
+
+c = a/b;
+printf("divisiom of two number is :-%f",c);
+
+
+}
